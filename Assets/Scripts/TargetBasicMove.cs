619dd06f12c0dfa7132e9c995191c11004fc8723
Miguel Angel Rodenas Balestrini <miguel_angel.rb@outlook.com>
2021-08-08 17:26:33 -0300

    first commit

---
+++ Assets/Scripts/TargetBasicMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBasicMove : MonoBehaviour
+{
+    [SerializeField]
+    float spinSpeed;
+    [SerializeField]
+    float motionMagnitude;
+    [SerializeField]
+    bool doSpin = false;
+    [SerializeField]
+    bool doMotion = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (doSpin)
+        {
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+        if (doMotion)
+        {
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }   
+    }
+}
